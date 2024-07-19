eb40141e2499c8f77670e312038279a3b2020550
Vishal <kumarv8292@gmail.com>
2024-07-19 11:33:09 +0530

    Add Project Files

---
+++ public/js/script.js
@@ -0,0 +1,35 @@
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(() => {
+  'use strict'
+
+  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  const forms = document.querySelectorAll('.needs-validation')
+
+  // Loop over them and prevent submission
+  Array.from(forms).forEach(form => {
+    form.addEventListener('submit', event => {
+      if (!form.checkValidity()) {
+        event.preventDefault()
+        event.stopPropagation()
+      }
+
+      form.classList.add('was-validated')
+    }, false)
+  })
+})()
+
+
+//Index.ejs file GST toggle button
+let taxSwitch = document.getElementById("flexSwitchCheckDefault");
+let taxInfo = document.getElementsByClassName("tax-info");
+taxSwitch.addEventListener("click", ()=>{
+  for(info of taxInfo){
+      if(info.style.display != "inline"){
+          info.style.display= "inline";
+      }
+      else{
+          info.style.display= "none";
+      }
+      
+  }
+});
---
+++ routes/review.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const router = express.Router({mergeParams : true});
+const wrapAsync = require("../utils/wrapAsync.js");
+const ExpressError = require("../utils/ExpressError.js");
+const Review = require("../models/review.js");
+const Listing = require("../models/listing.js");
+const { validateReview, isLoggedIn, isReviewAuthor } = require("../middleware");
+const reviewController = require("../controllers/reviews.js");
+
+// Post Route
+router.post("/", 
+isLoggedIn,
+validateReview, 
+wrapAsync(reviewController.createReview))
+  
+  //Delete Review
+router.delete("/:reviewId", isLoggedIn, isReviewAuthor, wrapAsync(reviewController.deleteReview));
+
+module.exports = router;
